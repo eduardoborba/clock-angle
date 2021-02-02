c156a517968019ece282cb741801ff957bdc271b
Eduardo Borba <eduardo@jobscore.com>
2021-02-01 23:52:54 -0300

    Fix tests

--- spec/clock_api_spec.rb
+++ spec/clock_api_spec.rb
@@ -1,23 +1,21 @@
 require "spec_helper"
 
-RSpec.describe ClockApi do
-  def app
-    ClockApi
+def app
+  ClockApi
+end
+
+describe 'GET /rest/clocks with hours and minutes' do
+  it 'returns the angle between the hours and minutes pointer' do
+    get '/rest/clock/6/0'
+
+    expect(last_response.status).to eq 200
+    expect(last_response.body).to eq({ angle: 180.0 }.to_json)
   end
 
-  describe 'GET /rest/clocks with hours and minutes' do
-    it 'returns the angle between the hours and minutes pointer' do
-      get '/rest/clock/6/0'
+  it 'returns the angle when minutes is not present' do
+    get '/rest/clock/6'
 
-      expect(last_response.status).to eq 200
-      expect(last_response.body).to eq({ angle: 180.0 }.to_json)
-    end
-
-    it 'returns the angle when minutes is not present' do
-      get '/rest/clock/6'
-
-      expect(last_response.status).to eq 200
-      expect(last_response.body).to eq({ angle: 180.0 }.to_json)
-    end
+    expect(last_response.status).to eq 200
+    expect(last_response.body).to eq({ angle: 180.0 }.to_json)
   end
 end
