84ca467bd40464d4325cd5e30b4ee8dc8cac34df
Eduardo Borba <eduardo@jobscore.com>
2021-02-01 23:59:00 -0300

    Remove unnecessary stuff and add new line to the end of files

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
-require "rubygems"
 require "bundler"
 Bundler.require(:default)                   # load all the default gems
-Bundler.require(Sinatra::Base.environment)  # load all the environment specific gems
 require './app/clock_api.rb'
 require './lib/clock.rb'
 require './models/angle.rb'
